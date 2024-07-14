0e70971336722e9617e46a41ced5779e9f902763
dennis-del <dennisjames991@gmail.com>
2024-07-15 03:25:10 +0530

    login responsive

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 
 function Register() {
   return (
-    <div className='login'>
-        <div className="login-container">
+    <div className='login w-100'>
+        <div className="login-container w-50">
             <h1>Sign In</h1>
-            <div className="login-fields">
+            <div className="login-fields w-100">
                 <input type="Email" placeholder='Email' className='text-white' />
                 <input type="password" placeholder='Password' className='text-white' />
             </div>
-            <button type='btn'>login</button>
+            <button type='button' className='w-100'>login</button>
             <p className='login-login'>Are you a new User? <Link to={'/login'} style={{textDecoration:'none'}}><span>Register</span></Link></p>
         </div>
     </div>
--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div className='login'>
-        <div className="login-container">
+    <div className='login w-100'>
+        <div className="login-container w-50">
             <h1>Sign Up</h1>
-            <div className="login-fields">
+            <div className="login-fields w-100">
                 <input type="text" placeholder='Your Name' className='text-white' />
                 <input type="Email" placeholder='Email' className='text-white' />
                 <input type="password" placeholder='Password' className='text-white' />
             </div>
-            <button type='btn'>Continue</button>
+            <button type='button' className='w-100'>Continue</button>
             <p className='login-login'>Already have an account? <Link to={'/register'} style={{textDecoration:'none'}}><span>Login Here</span></Link></p>
             <div className="login-agree">
                 <input type="checkbox" name="" id="" />
