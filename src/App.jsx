ca907193335d30ffea1d2dc429e92c365c687bc1
dennis-del <dennisjames991@gmail.com>
2024-07-14 20:36:49 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import Footer from './components/layout/footer/Footer'
 import Header from './components/layout/header/Header'
 import Landingpage from './components/main pages/landing page/Landingpage'
 import Home from './components/main pages/home/Home'
-<<<<<<< HEAD
 import Login from './pages/Login'
 import Register from './pages/Register'
-=======
 import Profile from './pages/Profile'
->>>>>>> 261df3014090f463357c01918646d3db0ccf137c
+
 
 
 
@@ -25,12 +23,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Landingpage/>} />
         <Route path='/home' element={<Home/>}/>
-<<<<<<< HEAD
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
-=======
         <Route path='/profile' element={<Profile/>}/>
->>>>>>> 261df3014090f463357c01918646d3db0ccf137c
       </Routes>
       <Footer/>
     </>
